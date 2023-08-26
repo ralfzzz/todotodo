5dd8c92cce04907a0df1d45cb8e9f1b4dfe61337
ralfzzz <azbimanyu@gmail.com>
2023-08-26 15:15:35 +0700

    feat(add&delete): create add and delete todo list logic

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 8003;
 
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.listen(port,() => {
     console.log(`node running on port ${port}`);
@@ -13,10 +15,32 @@ app.get('/tes',(req, res) => {
     res.send('ralfzzz ready!');
 });
 
+let todo = ["makan", "makan2"];
+let workTodo = ["kerja", "kerja2"];
+
 app.get('/', (req, res) => {
-    res.render('todo.ejs');
+    res.render('todo.ejs',{
+        'todo' : todo, 
+        'active': 'todo'
+    })
+})
+
+app.post('/add', (req, res, next) => {
+    let newTodo = req.body.todo;
+    todo.push(newTodo);
+    res.redirect('/')
+})
+
+app.post('/delete', (req,res, next) => {
+    // console.log(req.body.deletedTodo);
+    let deletedTodo = req.body.deletedTodo;
+    todo.splice(deletedTodo, 1);
+    res.redirect('/');
 })
 
 app.get('/work', (req, res) => {
-    res.render('todo.ejs');
+    res.render('todo.ejs', {
+        'todo': workTodo,
+        'active': 'work'
+    });
 })
